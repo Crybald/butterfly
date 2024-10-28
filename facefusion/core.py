9319e9a2f745f420079024d168893ce08d15b7cc
Crybald <crybald@gmail.com>
2024-10-28 13:47:17 -0300

    Update core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -120,8 +120,7 @@ def conditional_process() -> ErrorCode:
 			return 2
 	conditional_append_reference_faces()
 	if is_image(state_manager.get_item('target_path')):
-		#return process_image(start_time)
-		return process_video(start_time)
+		return process_image(start_time)
 	if is_video(state_manager.get_item('target_path')):
 		return process_video(start_time)
 	return 0
